62ffcb9da6f1c26d670ee4b7a4429155699ad9d4
Lautaro Angelico <angelico.lautaro@gmail.com>
2025-03-01 02:21:19 -0800

    Online characters cron fix

--- includes/cron/online_characters.php
+++ includes/cron/online_characters.php
@@ -15,15 +15,13 @@
 $file_name = basename(__FILE__);
 
 // load database
-$me = Connection::Database('Me_MuOnline');
-$charactersDB = config('SQL_DB_NAME', true);
-$accountsDB = config('SQL_USE_2_DB', true) == true ? config('SQL_DB_2_NAME', true) : $charactersDB;
+$mu = Connection::Database('MuOnline');
 
 $result = array();
 
 $query = "SELECT `"._CLMN_CHR_NAME_."` FROM `"._TBL_CHR_."` WHERE `"._CLMN_CHR_ONLINE_."` = 1";
 
-$onlineCharactersList = $me->query_fetch($query);
+$onlineCharactersList = $mu->query_fetch($query);
 if(is_array($onlineCharactersList)) {
 	foreach($onlineCharactersList as $onlineCharacterData) {
 		if(in_array($onlineCharacterData[_CLMN_CHR_NAME_], $result)) continue;
